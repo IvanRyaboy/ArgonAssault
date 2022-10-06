2e07f244b0b063b7a5f6e01762708839c2476618
IvanRyaboy <93194467+IvanRyaboy@users.noreply.github.com>
2022-10-06 12:05:16 +0300

    Enemy
    Add more enemy prefubs, updatehitpoints and RuntimePosition

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,14 +6,22 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] GameObject hitVFX;
-    [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
     [SerializeField] int hitPoints = 4;
     ScoreCounter scoreCounter;
+    GameObject parentGameObject;
 
     void Start()
     {
         scoreCounter = FindObjectOfType<ScoreCounter>();
+        parentGameObject = GameObject.FindWithTag("RuntimePosition");
+        AddRigidbody();
+    }
+
+    private void AddRigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     void OnParticleCollision(GameObject other)
@@ -29,7 +37,7 @@ public class Enemy : MonoBehaviour
      private void ProcessHit()
     {
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         hitPoints--;
         scoreCounter.CountScore(scorePerHit);
     }
@@ -37,7 +45,7 @@ public class Enemy : MonoBehaviour
     private void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(this.gameObject);
     }
 }
