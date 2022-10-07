3099a45913d10c924e564e8abb836ec05f6b7f26
IvanRyaboy <93194467+IvanRyaboy@users.noreply.github.com>
2022-10-07 11:36:44 +0300

    SFX
    Add music
    Also add enemy waves

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -39,7 +39,6 @@ public class Enemy : MonoBehaviour
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parentGameObject.transform;
         hitPoints--;
-        scoreCounter.CountScore(scorePerHit);
     }
 
     private void KillEnemy()
@@ -47,5 +46,6 @@ public class Enemy : MonoBehaviour
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parentGameObject.transform;
         Destroy(this.gameObject);
+        scoreCounter.CountScore(scorePerHit);
     }
 }
